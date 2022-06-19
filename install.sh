002cc8705591177d3ea392102e94b0466340d189
Anthony <techx@opayq.com>
2022-06-19 11:02:36 -0400

    update machine uname install script

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ LATEST_VERSION=$(curl -s -L https://api.github.com/repos/anthonygress/FireTV-Too
 
 install_app() {
     echo -e "\n---------------------- Installing Application ----------------------"
-    echo -e "Installing latest version: $LATEST_VERSION\n"
+    echo -e "Installing latest version: $LATEST_VERSION on $USER_PLATFORM\n"
 
     if [[ "$USER_PLATFORM" == "Darwin arm64" ]]
     then
         curl -sL https://github.com/AnthonyGress/FireTV-Toolkit/releases/download/v${LATEST_VERSION}/FireTV-Toolkit-${LATEST_VERSION}-arm64-mac.zip --output ~/Downloads/FireTV-Toolkit.zip && unzip -qo ~/Downloads/FireTV-Toolkit.zip -d /Applications
-    elif [[ "$USER_PLATFORM" == "Darwin x64" ]]
+    elif [[ "$USER_PLATFORM" == "Darwin x86_x64" ]]
     then
         curl -sL https://github.com/AnthonyGress/FireTV-Toolkit/releases/download/v${LATEST_VERSION}/FireTV-Toolkit-${LATEST_VERSION}-mac.zip --output ~/Downloads/FireTV-Toolkit.zip && unzip -qo ~/Downloads/FireTV-Toolkit.zip -d /Applications
     elif [[ "$USER_PLATFORM" == "Linux arm64" ]]
     then
          curl -sL https://github.com/AnthonyGress/FireTV-Toolkit/releases/download/v${LATEST_VERSION}/FireTV-Toolkit-${LATEST_VERSION}-arm64.AppImage --output ~/Desktop/FireTV-Toolkit-arm64.AppImage && chmod +x ~/Desktop/FireTV-Toolkit-arm64.AppImage
-    elif [[ "$USER_PLATFORM" == "Linux x64" ]]
+    elif [[ "$USER_PLATFORM" == "Linux x86_x64" ]]
     then
          curl -sL https://github.com/AnthonyGress/FireTV-Toolkit/releases/download/v${LATEST_VERSION}/FireTV-Toolkit-${LATEST_VERSION}.AppImage --output ~/Desktop/FireTV-Toolkit.AppImage && chmod +x ~/Desktop/FireTV-Toolkit.AppImage
     else
