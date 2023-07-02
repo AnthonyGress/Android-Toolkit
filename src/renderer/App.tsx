18d3f57264bd4ffd4b9c5e18ec8f0970bb0309c9
Anthony Gress <the.techx.guy@gmail.com>
2023-07-02 13:20:13 -0400

    allow console in prod win, fix terminal spacing win

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -27,6 +27,8 @@ const Main = () => {
         if (event.source === window && typeof event.data === 'string') {
             let stringData = JSON.stringify(event.data);
             stringData = stringData.replace(new RegExp('\\\\n', 'g'), '\n');
+            stringData = stringData.replace(new RegExp('\\\\r', 'g'), '\n');
+
             stringData = stringData.slice(1, -1);
             setTerminalOutput(stringData);
         }
